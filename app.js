367c03f8339e071127019cfb8538655f7763b092
Daniglez03 <gonzalezlorenzodani7@gmail.com>
2022-09-29 14:11:50 +0100

    Commit 2, casi finish

--- app.js
+++ app.js
@@ -1,30 +1,78 @@
 const args = process.argv.slice(2)
-console.log("Parámetros :", args);
 
-const loadNumber = () => {
-    args.forEach(value => {
-        let number = parseInt(value.substring(5))
-            return number
-    })
-}
+//Haver subString de (0, 5) para confirmar que empiece por ... "--op="
 
+if (args.length > 0) {
+    let arrNum = []
+    const loadNumber = () => {
+        args.slice(1).forEach(value => {
+            let number = Number(value.substring(5))
+            if (!isNaN(number) && number !== 0) {
+                arrNum.push(number)
+            }
+        })
+    }
+    loadNumber()
+    let signo = args[0].substring(12)
+    let resultado = {
+        operator: signo,
+        ops: arrNum,
+        res: 0
+    }
+    switch (args[0]) {
+        case "--operation=/":
+            if (arrNum.length >= 2) {
+                //Más resumido pero no se si se puede utilizar reducir
+                //const totalDiv = arrNum.reduce((p, c) => p / c)
+                let total = 0
+                total = arrNum[0] / arrNum[1]
+                console.log(total);
+                arrNum.slice(2).forEach(element => {
+                    total /= element
+                });
+                resultado.res = total
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser mayor que 1");
+            }
+            break;
+        case "--operation=*":
+            if (arrNum.length >= 2) {
+                //Más resumido pero no se si se puede utilizar reducir
+                //const totalMult = arrNum.reduce((p, c) => p * c)
+                let mult = 1
+                arrNum.forEach(element => {
+                    mult *= element
+                });
+                resultado.res = mult
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser mayor que 1");
+            }
+            break
+        case "--operation=^":
+            if (arrNum.length > 1 && arrNum.length < 3) {
+                const totalPotencia = arrNum[0] ** arrNum[1]
+                resultado.res = totalPotencia
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser 2");
+            }
+            break;
+        case "--operation=@":
+            if (arrNum.length === 1) {
+                const totalRaiz = Math.sqrt(arrNum[0])
+                resultado.res = totalRaiz
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser 1");
+            }
+            break
+        default:
+            console.log("ERROR: Parámetros erróneos");
+            break;
+    }
+} else {
 
-switch (args[0]) {
-    case "--operation=/":
-        let div = 0;
-        div = loadNumber() / loadNumber()
-        console.log("resultado: ", div);
-        break;
-    case "--operation=*":
-        console.log("*");
-        break
-    case "--operation=^":
-        console.log("^");
-        break;
-    case "--operation=@":
-        console.log("@");
-        break
-    default:
-        console.log("Parámetros erróneos");
-        break;
+    console.log("ERROR: No hay argumentos");
 }
